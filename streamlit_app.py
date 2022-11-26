80ae7bd0f473e231dc1f2bb6c2a2321bbee6798b
Girish1102 <87365928+Girish1102@users.noreply.github.com>
2022-11-26 20:45:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit;
-
-streamlit.title("My Name is Girish")
+   
+streamlit.title("My Parents New Healthy Dinner")
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🐔 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
